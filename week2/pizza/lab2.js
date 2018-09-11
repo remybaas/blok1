a2492c80819f69a6a14dc9d7646ad4ad360d0fe7
remy <remybaas22112000@gmail.com>
2018-09-11 10:22:02 +0200

    pizza calculator
    de werkende goede versie waar niks aan gedaan hoeft te worden

--- week2/pizza/lab2.js
+++ week2/pizza/lab2.js
@@ -1,23 +1,31 @@
-var small = 7.50;
-var total0 = prompt("how many small pizz'a do you want?");
 
+//totale prijzen van de pizza's 
 
-document.write("Small pizza's =" + total0);
-document.write("<br> Price = " + total0 * small);
+var smallPizzaPrice = 7.50;
+var mediumPizzaPrice = 9.50;
+var largePizzaPrice = 12.00;
 
-var medium = 9.50;
-var Medium = prompt("how many medium pizza's do you want?");
+//berekeningen voor pizza small
+var totalPriceSmall = prompt("how many small pizz'a do you want?");
 
-document.write("<br><br> Medium pizza's =" + Medium);
-document.write("<br> Price = " + Medium * medium);
+document.write("Small pizza's =" + totalPriceSmall);
+document.write("<br> Price = " + totalPriceSmall * smallPizzaPrice);
 
-var large = 12.00;
-var total2 = prompt("how many large pizza's do you want?");
+//berekeningen voor pizza medium 
+var totalPriceMedium = prompt("how many medium pizza's do you want?");
 
-document.write("<br><br> Large pizza's = " + total2);
-document.write("<br> Price = " + total2 * large);
+document.write("<br><br> Medium pizza's =" + totalPriceMedium);
+document.write("<br> Price = " + totalPriceMedium * mediumPizzaPrice);
+
+//berekeningen voor pizza Large
+var totalPriceLarge = prompt("how many large pizza's do you want?");
+
+document.write("<br><br> Large pizza's = " + totalPriceLarge);
+document.write("<br> Price = " + totalPriceLarge * largePizzaPrice);
 document.write("<br>");
 
-var total=total0+ Medium+total2;
+
+//berekening totaal 
+var total=smallPizzaPrice*totalPriceSmall + mediumPizzaPrice*totalPriceMedium + largePizzaPrice*totalPriceLarge;
 document.write("Price=" + total);
  
